82b223be56743c3768a3695d41b8af00e2a7f8e2
deepak-kr7 <deepaknsn7@gmail.com>
2026-07-04 22:34:14 +0530

    refactor(gateway): remove locals block and inline name strings

--- modules/gateway/main.tf
+++ modules/gateway/main.tf
@@ -7,24 +7,6 @@ resource "azurerm_public_ip" "appgw_pip" {
   tags                = var.tags
 }
 
-locals {
-  frontend_ip_configuration_name = "${var.appgw_name}-feip"
-  
-  frontend_port_name          = "${var.appgw_name}-port-80"
-  
-  netflix_backend_pool_name   = "${var.appgw_name}-netflix-beap"
-  starbucks_backend_pool_name = "${var.appgw_name}-starbucks-beap"
-  
-  netflix_http_setting_name   = "${var.appgw_name}-netflix-be-htst"
-  starbucks_http_setting_name = "${var.appgw_name}-starbucks-be-htst"
-  
-  netflix_listener_name       = "${var.appgw_name}-netflix-lstn"
-  starbucks_listener_name     = "${var.appgw_name}-starbucks-lstn"
-  
-  netflix_routing_rule_name   = "${var.appgw_name}-netflix-rtr"
-  starbucks_routing_rule_name = "${var.appgw_name}-starbucks-rtr"
-}
-
 resource "azurerm_application_gateway" "appgw" {
   name                = var.appgw_name
   resource_group_name = var.resource_group_name
@@ -42,30 +24,30 @@ resource "azurerm_application_gateway" "appgw" {
   }
 
   frontend_port {
-    name = local.frontend_port_name
+    name = "${var.appgw_name}-port-80"
     port = 80
   }
 
   frontend_ip_configuration {
-    name                 = local.frontend_ip_configuration_name
+    name                 = "${var.appgw_name}-feip"
     public_ip_address_id = azurerm_public_ip.appgw_pip.id
   }
 
   # Netflix Backend Pool
   backend_address_pool {
-    name         = local.netflix_backend_pool_name
+    name         = "${var.appgw_name}-netflix-beap"
     ip_addresses = var.netflix_backend_ips
   }
 
   # Starbucks Backend Pool
   backend_address_pool {
-    name         = local.starbucks_backend_pool_name
+    name         = "${var.appgw_name}-starbucks-beap"
     ip_addresses = var.starbucks_backend_ips
   }
 
   # HTTP Settings for Netflix (port 80 on VMs)
   backend_http_settings {
-    name                  = local.netflix_http_setting_name
+    name                  = "${var.appgw_name}-netflix-be-htst"
     cookie_based_affinity = "Disabled"
     port                  = 80
     protocol              = "Http"
@@ -74,7 +56,7 @@ resource "azurerm_application_gateway" "appgw" {
 
   # HTTP Settings for Starbucks (port 80 on VMs)
   backend_http_settings {
-    name                  = local.starbucks_http_setting_name
+    name                  = "${var.appgw_name}-starbucks-be-htst"
     cookie_based_affinity = "Disabled"
     port                  = 80
     protocol              = "Http"
@@ -83,39 +65,39 @@ resource "azurerm_application_gateway" "appgw" {
 
   # Listener for Netflix (port 80)
   http_listener {
-    name                           = local.netflix_listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
+    name                           = "${var.appgw_name}-netflix-lstn"
+    frontend_ip_configuration_name = "${var.appgw_name}-feip"
+    frontend_port_name             = "${var.appgw_name}-port-80"
     protocol                       = "Http"
     host_name                      = var.netflix_host_name
   }
 
   # Listener for Starbucks (port 80)
   http_listener {
-    name                           = local.starbucks_listener_name
-    frontend_ip_configuration_name = local.frontend_ip_configuration_name
-    frontend_port_name             = local.frontend_port_name
+    name                           = "${var.appgw_name}-starbucks-lstn"
+    frontend_ip_configuration_name = "${var.appgw_name}-feip"
+    frontend_port_name             = "${var.appgw_name}-port-80"
     protocol                       = "Http"
     host_name                      = var.starbucks_host_name
   }
 
   # Routing Rule for Netflix
   request_routing_rule {
-    name                       = local.netflix_routing_rule_name
+    name                       = "${var.appgw_name}-netflix-rtr"
     rule_type                  = "Basic"
-    http_listener_name         = local.netflix_listener_name
-    backend_address_pool_name  = local.netflix_backend_pool_name
-    backend_http_settings_name = local.netflix_http_setting_name
+    http_listener_name         = "${var.appgw_name}-netflix-lstn"
+    backend_address_pool_name  = "${var.appgw_name}-netflix-beap"
+    backend_http_settings_name = "${var.appgw_name}-netflix-be-htst"
     priority                   = 10
   }
 
   # Routing Rule for Starbucks
   request_routing_rule {
-    name                       = local.starbucks_routing_rule_name
+    name                       = "${var.appgw_name}-starbucks-rtr"
     rule_type                  = "Basic"
-    http_listener_name         = local.starbucks_listener_name
-    backend_address_pool_name  = local.starbucks_backend_pool_name
-    backend_http_settings_name = local.starbucks_http_setting_name
+    http_listener_name         = "${var.appgw_name}-starbucks-lstn"
+    backend_address_pool_name  = "${var.appgw_name}-starbucks-beap"
+    backend_http_settings_name = "${var.appgw_name}-starbucks-be-htst"
     priority                   = 20
   }
 
