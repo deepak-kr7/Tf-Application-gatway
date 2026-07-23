63f7413734bc58464fffdbb7d3fd36a95d5812d1
deepak-kr7 <deepaknsn7@gmail.com>
2026-07-24 00:00:36 +0530

    new backen

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -6,6 +6,14 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "demo_rg"
+    storage_account_name = "demostg1344"
+    container_name       = "democ"
+    key                  = "firstcicd.tfstate"
+  }
+
+
 }
 
 provider "azurerm" {
