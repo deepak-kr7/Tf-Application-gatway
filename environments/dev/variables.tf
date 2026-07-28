2388378caa34940757b643053497afa114cb5ae8
deepak-kr7 <deepaknsn7@gmail.com>
2026-07-28 20:27:35 +0530

    update

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -2,15 +2,23 @@ variable "environment" {}
 variable "admin_username" {}
 variable "tags" {}
 variable "admin_password" {}
-variable "ssl_certificate_pfx_base64" {}
-variable "ssl_certificate_password" {}
+variable "ssl_certificate_pfx_base64" {
+  type    = string
+  default = ""
+}
+variable "ssl_certificate_password" {
+  type    = string
+  default = ""
+}
 variable "resource_groups" {}
 variable "vnets" {}
 variable "subnets" {}
 variable "virtual_machines" {}
 variable "nsgs" {}
 variable "bastions" {}
-variable "waf_policies" {}
+variable "waf_policies" {
+  default = {}
+}
 variable "gateways" {}
 variable "aks_clusters" {}
 variable "container_registries" {}
